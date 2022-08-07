f207cb7cf2457002036d0be788d8effae08253ac
Azralmukmin Azmi <azral_m@yahoo.com>
2022-08-07 12:10:04 +0800

    Add files via upload

---
+++ ntpdatetime/ntpdatetime.ino
@@ -0,0 +1,65 @@
+#include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "azralPhone";
+const char* password = "azral123";
+
+const long utcOffsetInSeconds = 8 * 60 * 60;
+char daysOfTheWeek[7][12] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  connectWifi();
+
+  timeClient.begin();
+}
+
+void loop() {
+  delay(1000);
+
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWifi();
+  }
+
+  printLocalTime();
+}
+
+void connectWifi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  int resetCount = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    resetCount++;
+    if (resetCount > 10) {
+      ESP.restart();
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void printLocalTime() {
+  timeClient.update();
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
+  Serial.println(timeClient.getFormattedTime());
+}
